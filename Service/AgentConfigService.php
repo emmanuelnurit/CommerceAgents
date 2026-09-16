a356d6e166a5d431e67f1f5168342c5002a2614c
MyOrg CTO <emmanuel.nurit@gmail.com>
2026-09-16 08:06:08 +0200

    test(AC2): commit fonctionnel jetable pour re-verif CTO MYO-454 (a nettoyer)

--- Service/AgentConfigService.php
+++ Service/AgentConfigService.php
@@ -258,3 +258,4 @@ final readonly class AgentConfigService
         return array_values(array_filter(array_map('intval', explode(',', $raw))));
     }
 }
+// MYO-454 CTO re-verif AC2 2026-09-16T06:06:08Z
