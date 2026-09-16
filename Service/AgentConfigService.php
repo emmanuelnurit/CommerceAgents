a97888e91034d5b6e3ea94b33348c8bdc3c4795b
MyOrg CTO <emmanuel.nurit@gmail.com>
2026-09-16 08:37:47 +0200

    Revert "test(AC4): commit fonctionnel jetable 3/3 pour prouver le diffstat MYO-465 (a nettoyer via revert)"
    This reverts commit e7fbb6da2e6f6aa11cafef399cc5a6f1004a341d.

--- Service/AgentConfigService.php
+++ Service/AgentConfigService.php
@@ -260,4 +260,3 @@ final readonly class AgentConfigService
 }
 // test AC4 MYO-465 - commit jetable 1/3, a nettoyer via revert
 // test AC4 MYO-465 - commit jetable 2/3, a nettoyer via revert
-// test AC4 MYO-465 - commit jetable 3/3, a nettoyer via revert
